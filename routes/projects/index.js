200b355b6caee40ed90bc22262e1fffb84bde470
Akshit Bansal <akshit.bansal755@gmail.com>
2021-02-19 10:33:51 +0530

    added delete project functionality

--- routes/projects/index.js
+++ routes/projects/index.js
@@ -4,6 +4,7 @@ const middleware = require('../../middleware');
 const wrapAsync = require('../../utils/wrapAsync');
 const Project = require('../../models/Project');
 const filesRouter = require('./files');
+const File = require('../../models/File');
 
 router.get('/',middleware.ensureLogin,wrapAsync(async (req,res) => {
     await req.user.populate('projects').execPopulate();
@@ -25,6 +26,12 @@ router.post('/',middleware.ensureLogin,wrapAsync(async (req,res) => {
 router.get('/:id',middleware.ensureLogin,middleware.findProject,middleware.authorizeProject,wrapAsync(async (req,res) => {
     await req.projectQuery.populate('files').execPopulate();
     res.render('projects/show',{project: req.projectQuery});
+}))
+
+router.delete('/:id',middleware.ensureLogin,middleware.findProject,middleware.authorizeProject,wrapAsync(async (req,res) => {
+    const project = await Project.findByIdAndDelete(req.params.id);
+    for(const file of project.files) await File.deleteOne({_id: file});
+    res.redirect('/projects');
 }))
 
 router.use('/:id/files',filesRouter);
