aac9fc212fe48bfd722724fb7cb60ead38588303
Akshit Bansal <akshit.bansal755@gmail.com>
2021-02-23 16:13:54 +0530

    added ace text-editor

--- public/js/projects/show.js
+++ public/js/projects/show.js
@@ -3,23 +3,24 @@ const vPillsTabContent = document.querySelector('#v-pills-tab-content');
 const addFileForm = document.querySelector('#add-file-form');
 const consoleTextarea = document.querySelector('#console-textarea');
 const runProjectForm = document.querySelector('#run-project-form');
+const runProjectBtn = runProjectForm.querySelector('button');
 const addFileName = document.querySelector('#add-file-name');
 const addFileExtension = document.querySelector('#add-file-extension');
 const newFileModalClose = document.querySelector('#new-file-modal-close');
 
 async function getAllFilesRequest(){
-    return axios.get(`/api/projects/${id}/files`);
+    return axios.get(`/api/projects/${project._id}/files`);
 }
 
 async function getFileRequest(fileId){
-    return axios.get(`/api/projects/${id}/files/${fileId}`);
+    return axios.get(`/api/projects/${project._id}/files/${fileId}`);
 }
 
 async function createFileRequest(name,extension){
     const body = new URLSearchParams();
     body.append('name',name);
     body.append('extension',extension);
-    return axios.post(`/api/projects/${id}/files`,body.toString());
+    return axios.post(`/api/projects/${project._id}/files`,body.toString());
 }
 
 async function updateFileRequest(fileId,name,extension,data){
@@ -27,15 +28,15 @@ async function updateFileRequest(fileId,name,extension,data){
     body.append('name',name);
     body.append('extension',extension);
     body.append('data',data);
-    return axios.put(`/api/projects/${id}/files/${fileId}`,body.toString());
+    return axios.put(`/api/projects/${project._id}/files/${fileId}`,body.toString());
 }
 
 async function deleteFileRequest(fileId){
-    return axios.delete(`/api/projects/${id}/files/${fileId}`);
+    return axios.delete(`/api/projects/${project._id}/files/${fileId}`);
 }
 
 async function runProject(){
-    return axios.get(`/api/projects/${id}/run`);
+    return axios.get(`/api/projects/${project._id}/run`);
 }
 
 function addFileToPage(file){
@@ -49,6 +50,8 @@ function addFileToPage(file){
     btn.setAttribute('data-bs-toggle','pill');
     btn.setAttribute('data-bs-target',`#v-pills-${file._id}`);
     btn.textContent = file.name;
+    const unsaveDot = document.createElement('sup');
+    unsaveDot.innerHTML = ' <i class="bi bi-circle-fill text-warning"></i>';
     btn.style.boxShadow = '0px 0px 21px -2px #0000001f';
     vPillsTab.append(btn);
 
@@ -62,27 +65,47 @@ function addFileToPage(file){
                 <span class="mx-2">.</span>
                 <input type="text" class="form-control" name="extension" value="${extension}" list="types-datalist">
             </div>
-            <textarea name="data" class="form-control mb-2" rows="10">${file.data}</textarea>
-            <button class="btn btn-primary">Save</button>
-        </form>
-        <form class="mt-2 delete-form">
-            <button class="btn btn-danger">Delete</button>
-        </form`;
+            <div class="mb-2" id="editor-${file._id}" style="height: 500px; border: 1px solid #ced4da; border-radius: .25rem; font-size: 1rem;"></div>
+            <button class="btn btn-primary btn-save">Save</button>
+            <button class="btn btn-danger btn-delete">Delete</button>
+        </form>`;
     const nameInput = tab.querySelector('input[name="name"]');
     const extensionInput = tab.querySelector('input[name="extension"]');
-    const dataInput = tab.querySelector('textarea');
+    const saveBtn = tab.querySelector('.btn-save');
+    const editorDiv = tab.querySelector(`#editor-${file._id}`);
+    const editor = ace.edit(editorDiv);
+    editor.session.setValue(file.data);
+    if(project.type === 'c++') editor.session.setMode('ace/mode/c_cpp');
+    else if(project.type === 'nodejs') editor.session.setMode('ace/mode/javascript');
+    editor.session.on('change',() => {
+        btn.append(unsaveDot);
+    })
+    editor.setOptions({
+        enableBasicAutocompletion: true,
+        enableSnippets: true,
+        enableLiveAutocompletion: false
+    });
     tab.querySelector('form.save-form').addEventListener('submit',async e => {
         e.preventDefault();
-        await updateFileRequest(file._id,nameInput.value,extensionInput.value,dataInput.value);
+        await updateFileRequest(file._id,nameInput.value,extensionInput.value,editor.getValue());
         btn.textContent = `${nameInput.value}.${extensionInput.value}`;
+        unsaveDot.remove();
     })
-    tab.querySelector('form.delete-form').addEventListener('submit',async e => {
-        e.preventDefault();
+    tab.querySelector('.btn-delete').addEventListener('click',async () => {
         await deleteFileRequest(file._id);
         btn.remove();
         tab.remove();
     })
     vPillsTabContent.append(tab);
+    editorDiv.addEventListener('keydown',e => {
+        if(e.code === 'KeyS' && e.ctrlKey){
+            saveBtn.click();
+            e.preventDefault();
+        }else if(e.code === 'KeyR' && e.ctrlKey){
+            runProjectBtn.click();
+            e.preventDefault();
+        }
+    })
 }
 
 addFileForm.addEventListener('submit',async e => {
